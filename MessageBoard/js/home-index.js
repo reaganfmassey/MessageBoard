796a5aa6e30544857ec289714517607a3d535af0
vishnumallipudi <vishnumallipudi.584@gmail.com>
2017-08-08 17:23:20 -0700

    added angularjs and retrieved data and presented it in views

---
+++ MessageBoard/js/home-index.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module("myapp",[]);
+
+
+app.controller("homeIndexController", function ($scope,$http) {
+    //alert("In the homeIndexController");
+
+    $scope.dataCount = 0;
+    $scope.data = [];
+    $scope.isBusy = true;
+    $http.get("/api/topics/?includeReplies=true")
+        .then(function (result) {
+            // api successfuly called
+            angular.copy(result.data, $scope.data);
+            $scope.dataCount = result.data.length;
+        },
+        function () {
+            //error
+            alert("could not load topics");
+        })
+        .then(function () {
+            $scope.isBusy = false;
+        });
+});
